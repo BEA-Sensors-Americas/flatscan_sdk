d4b21154c2c5a4363ec2027e83474c7edfce4d27
jlibeainc <ljy.jiangyue@gmail.com>
2023-11-30 11:08:49 -0500

    refactor

--- device_type/controller.py
+++ device_type/controller.py
@@ -7,12 +7,16 @@ import numpy as np
 
 
 class Controller:
-    def __init__(self, port_number, buffer_maximum_length=100, baudrate=BAUDRATE_DEFAULT):
-        self.device = Flatscan(port_number, baudrate=baudrate)
-        self.upper_device=Flatscan(7, baudrate=baudrate)
+    def __init__(self, port_number1, buffer_maximum_length=100, baudrate=BAUDRATE_DEFAULT):
+        self.device = Flatscan(port_number1, baudrate=baudrate)
+
+    def __init__(self, port_number1, port_number2, buffer_maximum_length=100, baudrate=BAUDRATE_DEFAULT):
+        self.device = Flatscan(port_number1, baudrate=baudrate)
+        self.upper_device = Flatscan(port_number2, baudrate=baudrate)
 
     """Two sensors one zone on upper sensor, rectangular left, right, front zones on lower sensor
     return priority give to upper sensor"""
+
     def update_occupancy(self):
         mdi = self.device.get_mdi()
 
@@ -20,13 +24,15 @@ class Controller:
         # print(distances)
         right_angel_point = (len(distances) / (self.device.parameters_state['angle_last']
                                                - self.device.parameters_state['angle_first'])) * 9000
+
         rec_cor = utils.convert_distances_to_cartesian(distances, math.floor(right_angel_point))
         occupancy_left = utils.cartisian_get_in_rec(rec_cor, 450, 700)
         occupancy_middle = utils.cartisian_get_in_rec(rec_cor, 1100, 1000)
         occupancy_right = utils.cartisian_get_in_rec(rec_cor, 1400, 700, depth_min=1100, width_min=0)
         occupancy_front = utils.cartisian_get_in_rec(rec_cor, 1400, 260)
-        upper_distances=self.upper_device.get_mdi()
-        upper_occupancy=utils.get_presence(upper_distances, 1400)
+
+        upper_distances = self.upper_device.get_mdi()
+        upper_occupancy = utils.get_presence(upper_distances, 1400)
         if not upper_occupancy:
             return 3
         elif occupancy_front:
@@ -44,6 +50,7 @@ class Controller:
 
     """Two sensors multiple zone on upper sensors, rectangular left, right, front zones on lower sensor
     no return priority all the same"""
+
     def update_occupancy_zones(self, num_zones):
         mdi = self.device.get_mdi()
 
--- subwaysurfer.py
+++ subwaysurfer.py
@@ -4,33 +4,25 @@ from device_type.rad_distance_device import *
 from device_type.controller import *
 import pyautogui as pg
 
-
-flatscan = Controller(4)
-state = 2
-count=0
+flatscan = Controller(4, 7)
 while True:
     new_state = flatscan.update_occupancy()
     if new_state != state:
         state = new_state
         if state == 0:
             pg.press('up')
-            #print("up")
+            # print("up")
         elif state == 1:
             pg.press('left')
-            #print("left")
+            # print("left")
         elif state == 3:
             pg.press('down')
-            #print("down")
+            # print("down")
         elif state == 2:
             pass
-            #print("MIDE")
+            # print("MIDE")
         elif state == 6:
             pg.press('space')
         elif state == 4:
             pg.press('right')
-            #print("right")
-
-
-
-
-
+            # print("right")
